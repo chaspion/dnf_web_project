248b81154ca9915ca0663d2bc54af8c445fee1aa
chaspion <49503342+chaspion@users.noreply.github.com>
2020-08-28 19:01:53 +0900

    update

--- test.py
+++ test.py
@@ -11,6 +11,12 @@ from bs4 import BeautifulSoup
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.addArguments("start-maximized")
+# chrome_options.addArguments("disable-infobars")
+# chrome_options.addArguments("--disable-extensions")
+# chrome_options.addArguments("--disable-gpu")
+# chrome_options.addArguments("--no-sandbox")
 driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
 driver.get('https://dnf.akaib.com/')
 html = driver.page_source
